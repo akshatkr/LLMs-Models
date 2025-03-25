c5bd3e28fa93fab899ff84556c897ccb4917af48
Akshat <akshat.kumar@gmail.com>
2025-03-25 12:51:08 +0800

    na

--- llm_model_gpt4.py
+++ llm_model_gpt4.py
@@ -1,26 +1,30 @@
 import os
 import traceback
-import ollama
-import tenacity
+
+from config import get_openai_key
+
+import openai
+from openai import OpenAI
 from wrapt_timeout_decorator import timeout
-from ollama import Client
-
-
-# Set Ollama host
-os.environ["OLLAMA_HOST"] = "http://localhost:11434"
-
+import tenacity
 
 
 @timeout(dec_timeout=30, use_signals=False)
-def connect_ollama(engine, messages, temperature, max_tokens,
-                   top_p):
+def connect_openai(client, engine, messages, temperature, max_tokens,
+                   top_p, frequency_penalty, presence_penalty, stop):
     try:
-        return ollama.chat(
+        return client.chat.completions.create(
             model=engine,
-            messages=messages
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            stop=stop
         )
     except Exception as e:
-        print(f'[ERROR] Ollama model error: {e}')
+        print(f'[ERROR] OpenAI model error: {e}')
         raise e
 
 
@@ -28,6 +32,8 @@ class GPT_Chat:
     def __init__(self, engine, stop=None, max_tokens=1000, temperature=0, top_p=1,
                  frequency_penalty=0.0, presence_penalty=0.0):
         self.engine = engine
+        print(get_openai_key())
+        self.gpt_client = OpenAI(api_key=get_openai_key())
         self.max_tokens = max_tokens
         self.temperature = temperature
         self.top_p = top_p
@@ -37,7 +43,9 @@ class GPT_Chat:
 
     def get_response(self, prompt, messages=None, max_retry=5, verbose=False):
         conn_success, llm_output = False, ''
-        if messages is None:
+        if messages is not None:
+            messages = messages
+        else:
             messages = [{'role': 'user', 'content': prompt}]
 
         try:
@@ -47,15 +55,19 @@ class GPT_Chat:
                 reraise=True
             )
             response = r.__call__(
-                connect_ollama,
+                connect_openai,
+                client=self.gpt_client,
                 engine=self.engine,
                 messages=messages,
                 temperature=self.temperature,
                 max_tokens=self.max_tokens,
-                top_p=self.top_p)
-            llm_output = response["message"]["content"]
+                top_p=self.top_p,
+                frequency_penalty=self.freq_penalty,
+                presence_penalty=self.presence_penalty,
+                stop=self.stop)
+            llm_output = response.choices[0].message.content
             if verbose:
-                print(f'[INFO] Connection success')
+                print(f'[INFO] Connection success - token usage: {response.usage}')
             conn_success = True
         except Exception:
             print(traceback.format_exc())
@@ -64,10 +76,12 @@ class GPT_Chat:
 
 
 def main():
-    llm_gpt = GPT_Chat(engine='gemma3:4b')
-    _, llm_output = llm_gpt.get_response('Do you know about PDDL language for planning?')
+    llm_gpt = GPT_Chat(engine='gpt-4')
+    _, llm_output = llm_gpt.get_response('test')
     print(llm_output)
 
 
 if __name__ == '__main__':
     main()
+
+
